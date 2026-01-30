43456dbc09a2d2041b4412a93a1876e5bad77efb
Todor Ivanov <todor.trendafilov.ivanov@cern.ch>
2026-01-30 11:03:02 +0100

    Add WMCore runtime code

--- env.sh
+++ env.sh
@@ -1,1 +1,1 @@
-export PYTHONPATH=$PWD/src/python/WMCore.zip:$PWD/src/python:$PWD:$PYTHONPATH
+export PYTHONPATH=$PWD/src/python/WMCore.zip:$PWD/src/python/PSetTweaks.zip:$PWD/src/python/Utils.zip:$PWD/src/python:$PWD:$PYTHONPATH
